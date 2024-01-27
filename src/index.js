4b288cffc0fb2b790b97b5e455ac13348285a03b
late-bloomer82 <nateriver72@outlook.com>
2024-01-26 19:40:09 -0500

    imported the tab functions and attached them to the tab(buttons_ event listeners for website functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { loadContent } from "./pageLoad";
+import { createMenu } from "./pageLoad";
+import { createAbout } from "./pageLoad";
 
-document.addEventListener('DOMContentLoaded',loadContent());
+
+const homeButton = document.getElementById('home');
+const menuButton = document.getElementById('menu');
+const aboutButton = document.getElementById('about');
+
+document.addEventListener('DOMContentLoaded',loadContent);
+
+homeButton.addEventListener('click', loadContent);
+
+menuButton.addEventListener('click', createMenu);
+
+aboutButton.addEventListener('click', createAbout);
