63f2e571ff10b08e18748853b0264dd5772f975c
late-bloomer82 <nateriver72@outlook.com>
2024-01-26 20:29:23 -0500

    I added the loaders and plugins necessary for webpack to bundle up my css file and images with the main js file

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images', // Output path within the 'dist' directory
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
